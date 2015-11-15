7e554140a3c5d7d8b0bc603e0bb69f9367f3728d
Sriya Sarkar <sriyasarkar7@gmail.com>
2015-11-14 20:25:11 -0500

    let's try

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
+var _ = require('lodash');
+var Client = require('node-rest-client').Client;
 var Twit = require('twit');
 var natural = require('natural');
 //var twitInfo = require('config.js');
 //var twitter = new Twit(twitInfo);
 var fs = require('fs');
 var twitter = new Twit ({
-		consumer_key: 'cjyo4tD7o22xhS29PI3RcW5EO',
-		consumer_secret: 'MIMmDCsjyWJogp8cIOEBel0KgjJL6KPimf9VPZy648bvssCzZ4',
-		access_token: '3875093548-LC2okRvptebpkYrOikRqzUspM7E7Xu0dc9ppURv',
-		access_token_secret: '7SDKsOKwAPE8xxVRgzJfI2TVeSlim3oSSTpIJI1Dvw24i'
+		consumer_key: process.env.CONSUMER_KEY,
+		consumer_secret: process.env.CONSUMER_SECRET,
+		access_token: process.env.ACCESS_TOKEN,
+		access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
 
 tokenizer = new natural.WordTokenizer();
@@ -160,7 +162,7 @@ stream.on('tweet', function (tweet) {
 //runs the program once an hour
 setInterval (function() {
 	try {
-		run();
+		stream.on();
 	}
 	catch (e) {
 		console.log(e);
