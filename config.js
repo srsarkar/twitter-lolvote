b23db340a778facb1d12a69cdc65783574859f76
Sriya Sarkar <sriyasarkar7@gmail.com>
2016-04-16 14:23:50 -0400

    key changes
    key changes

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	consumer_key:'cjyo4tD7o22xhS29PI3RcW5EO',
+	consumer_secret:'MIMmDCsjyWJogp8cIOEBel0KgjJL6KPimf9VPZy648bvssCzZ4',
+	access_token:'3875093548-LC2okRvptebpkYrOikRqzUspM7E7Xu0dc9ppURv',
+	access_token_secret:'7SDKsOKwAPE8xxVRgzJfI2TVeSlim3oSSTpIJI1Dvw24i'
+}
